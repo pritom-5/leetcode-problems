653a5f38cbae3f7b8c353579b772a5e5fede95db
pritom-lap <iamsakibshadmanpritom@gmail.com>
2024-05-13 00:02:56 +0600

    ___

--- go/array/array.go
+++ go/array/array.go
@@ -1,6 +1,7 @@
 // ?????
 // what is the difference between []int{} and make([]int, 0) and var name []int
 // difference between byte and rune
+// map[string]struct{} i dont want to store any value in map
 
 package array
 
@@ -8,7 +9,7 @@ import (
 	"strings"
 )
 
-func ContainsDuplicate(nums []int) bool  {
+func ContainsDuplicate(nums []int) bool {
 	nums_map := make(map[int]int)
 
 	for _, num := range nums {
@@ -16,7 +17,7 @@ func ContainsDuplicate(nums []int) bool  {
 
 		if ok {
 			return true
-		} 
+		}
 
 		nums_map[num] = 1
 	}
@@ -24,7 +25,7 @@ func ContainsDuplicate(nums []int) bool  {
 	return false
 }
 
-func isAllItemsOfArrayZero (arr [26]int) bool {
+func isAllItemsOfArrayZero(arr [26]int) bool {
 	for _, item := range arr {
 		if item != 0 {
 			return false
@@ -33,8 +34,8 @@ func isAllItemsOfArrayZero (arr [26]int) bool {
 	return true
 }
 
-func ValidAnagram (s, t string) bool {
-	if (len(s) != len(t)) {
+func ValidAnagram(s, t string) bool {
+	if len(s) != len(t) {
 		return false
 	}
 
@@ -42,37 +43,36 @@ func ValidAnagram (s, t string) bool {
 	t_arr := []rune(t)
 	var letters_arr [26]int
 
-
 	for i, s_letter := range s_arr {
-		letters_arr[s_letter - 'a'] += 1
-		letters_arr[t_arr[i] - 'a'] -= 1
+		letters_arr[s_letter-'a'] += 1
+		letters_arr[t_arr[i]-'a'] -= 1
 	}
 
 	return isAllItemsOfArrayZero(letters_arr)
 
 }
 
-func ConcatenationOfArray (nums []int) []int {
-	new_nums := make([]int, len(nums) * 2)
+func ConcatenationOfArray(nums []int) []int {
+	new_nums := make([]int, len(nums)*2)
 
 	for i, num := range nums {
 		new_nums[i] = num
-		new_nums[len(nums) + i] = num
+		new_nums[len(nums)+i] = num
 	}
 
 	return new_nums
 }
 
-func GreatestRightSide (arr []int) []int {
+func GreatestRightSide(arr []int) []int {
 	output_arr := make([]int, len(arr))
-	greatest_item := arr[len(arr) - 1]
+	greatest_item := arr[len(arr)-1]
 
-	output_arr[len(arr) - 1] = -1 
+	output_arr[len(arr)-1] = -1
 
 	for i := len(arr) - 2; i >= 0; i-- {
 		output_arr[i] = greatest_item
 		num := arr[i]
-		if (num > greatest_item) {
+		if num > greatest_item {
 			greatest_item = num
 		}
 	}
@@ -81,17 +81,16 @@ func GreatestRightSide (arr []int) []int {
 
 }
 
-
-func IsSubsequence (s, t string) bool {
+func IsSubsequence(s, t string) bool {
 	left, right := 0, 0
 
-	for (left <= right && left < len(s) && right < len(t)){
+	for left <= right && left < len(s) && right < len(t) {
 		left_ch := s[left]
 		right_ch := t[right]
 
 		// fmt.Printf("left_ch: %d,right_ch: %d",left_ch,right_ch)
 
-		if (left_ch == right_ch) {
+		if left_ch == right_ch {
 			left += 1
 		}
 		right += 1
@@ -100,31 +99,28 @@ func IsSubsequence (s, t string) bool {
 	return left == len(s)
 }
 
-
-
-func LengthOfLastWord (s string) int {
+func LengthOfLastWord(s string) int {
 	trimed_s := strings.Trim(s, " ")
 	split_s_slice := strings.Split(trimed_s, " ")
-	return len(split_s_slice[len(split_s_slice) - 1])
+	return len(split_s_slice[len(split_s_slice)-1])
 }
-
 
 func TwoSum(nums []int, target int) [2]int {
 	for i := 0; i < len(nums); i++ {
 		for j := 1; j < len(nums); j++ {
-			if (nums[i] + nums[j] == target) {
+			if nums[i]+nums[j] == target {
 				return [2]int{i, j}
 			}
 		}
 	}
 	return [2]int{}
-} 
+}
 
-func smallestWord (strs []string) string {
+func smallestWord(strs []string) string {
 	smallest_word := strs[0]
 
 	for _, str := range strs {
-		if (len(str) < len(smallest_word)) {
+		if len(str) < len(smallest_word) {
 			smallest_word = str
 		}
 	}
@@ -139,7 +135,7 @@ func LongestCommonPrefix(strs []string) string {
 	for i, letter := range smallest_word {
 		for _, str := range strs {
 
-			if (rune(str[i]) != letter) {
+			if rune(str[i]) != letter {
 				return prefix
 			}
 		}
@@ -149,22 +145,22 @@ func LongestCommonPrefix(strs []string) string {
 	return prefix
 }
 
-func PascalsTriangle (numRows int) [][]int {
+func PascalsTriangle(numRows int) [][]int {
 	var pascals_triangle [][]int = [][]int{{1}}
 
 	for i := 1; i < numRows; i++ {
-		prev_row := pascals_triangle[i - 1]
-		curr_row := make([]int, i + 1)
+		prev_row := pascals_triangle[i-1]
+		curr_row := make([]int, i+1)
 		for j := 0; j <= i; j++ {
 			var val_1, val_2 int
 
-			if (j <= 0) {
+			if j <= 0 {
 				val_1 = 0
 			} else {
-				val_1 = prev_row[j - 1]
+				val_1 = prev_row[j-1]
 			}
 
-			if (j >= len(prev_row)) {
+			if j >= len(prev_row) {
 				val_2 = 0
 			} else {
 				val_2 = prev_row[j]
@@ -179,9 +175,154 @@ func PascalsTriangle (numRows int) [][]int {
 
 }
 
+func swap(nums []int, i, j int) {
+	tmp := nums[i]
+	nums[i] = nums[j]
+	nums[j] = tmp
+}
 
+func RemoveElement(nums []int, val int) []int {
+	left, right := 0, len(nums)-1
 
+	for left <= right {
+		if nums[left] == val {
+			swap(nums, left, right)
+			right -= 1
+			continue
+		}
+		left += 1
+	}
 
+	return nums
+}
 
+func strippedEmail(email string) string {
+	parts := strings.Split(email, "@")
+	should_break := false
 
+	updated_email_first_part := ""
 
+	for _, letter := range parts[0] {
+		switch letter {
+		case '.':
+			continue
+		case '+':
+			should_break = true
+		default:
+			updated_email_first_part += string(letter)
+		}
+
+		if should_break {
+			break
+		}
+	}
+
+	return updated_email_first_part + "@" + parts[1]
+}
+
+func strippedEmail_2(email string) string {
+	parts := strings.Split(email, "@")
+	local, domain := parts[0], parts[1]
+
+	local = strings.ReplaceAll(local, ".", "")
+	plus_idx := strings.Index(local, "+")
+
+	if plus_idx >= 0 {
+		local = local[:plus_idx]
+	}
+
+	return local + "@" + domain
+}
+
+func ValidEmails(emails []string) int {
+	email_map := make(map[string]int)
+	counter := 0
+
+	for _, email := range emails {
+		stripped_email := strippedEmail_2(email)
+		_, ok := email_map[stripped_email]
+		if !ok {
+			email_map[stripped_email] = 1
+			counter += 1
+		}
+	}
+	return counter
+}
+
+func getLetterMap(s, t string) map[rune]rune {
+	m := make(map[rune]rune)
+
+	for i, l := range s {
+		m[l] = rune(t[i])
+	}
+
+	return m
+}
+
+func IsomorphicStrings(s, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+
+	s_to_t_map := make(map[rune]rune)
+	t_to_s_map := make(map[rune]rune)
+
+	for i, l := range s {
+		_, ok_s := s_to_t_map[l]
+
+		if ok_s {
+			if s_to_t_map[l] != rune(t[i]) {
+				return false
+			}
+		} else {
+			s_to_t_map[l] = rune(t[i])
+		}
+
+		_, ok_t := t_to_s_map[rune(t[i])]
+
+		if ok_t {
+			if t_to_s_map[rune(t[i])] != l {
+				return false
+			}
+		} else {
+			t_to_s_map[rune(t[i])] = l
+		}
+	}
+
+	return true
+}
+
+func CanPlantFlower(flowerbed []int, n int) bool {
+	counter := 0
+
+	updateCounter := func(idx int) {
+		counter += 1
+		flowerbed[idx] = 1
+
+	}
+
+	for i := 0; i < len(flowerbed); i++ {
+		switch i {
+
+		case 0:
+			if len(flowerbed) == 1 && flowerbed[i] == 0 {
+				updateCounter(i)
+			} else if flowerbed[i] == 0 && flowerbed[i+1] == 0 {
+				updateCounter(i)
+			}
+
+		case len(flowerbed) - 1:
+			if flowerbed[i] == 0 && flowerbed[i-1] == 0 {
+				updateCounter(i)
+			}
+
+		default:
+			if flowerbed[i] == 0 && flowerbed[i-1] == 0 && flowerbed[i+1] == 0 {
+				updateCounter(i)
+			}
+		}
+
+	}
+
+	return counter >= n
+}
--- go/array/array_test.go
+++ go/array/array_test.go
@@ -6,12 +6,12 @@ import (
 	"testing"
 )
 
-func TestContainsDuplicate(t *testing.T)  {
-	contains_duplicate_test_items := []struct{
-		id int
-		nums []int
+func TestContainsDuplicate(t *testing.T) {
+	contains_duplicate_test_items := []struct {
+		id     int
+		nums   []int
 		expect bool
-	} {
+	}{
 		{1, []int{1, 2, 3, 1}, true},
 		{2, []int{1, 2, 3}, false},
 		{3, []int{1, 2, 3, 4}, false},
@@ -27,15 +27,15 @@ func TestContainsDuplicate(t *testing.T)  {
 
 		})
 	}
-	
+
 }
 
-func TestValidAnagram (t *testing.T) {
+func TestValidAnagram(t *testing.T) {
 
 	valid_anagram_test_items := []struct {
-		id int
+		id   int
 		s, t string
-		exp bool
+		exp  bool
 	}{
 		{1, "anagram", "nagaram", true},
 		{2, "rat", "car", false},
@@ -45,7 +45,7 @@ func TestValidAnagram (t *testing.T) {
 	for _, item := range valid_anagram_test_items {
 		t.Run(strconv.Itoa(item.id), func(t *testing.T) {
 			got := ValidAnagram(item.s, item.t)
-			if (got != item.exp) {
+			if got != item.exp {
 				t.Errorf("got: %t, exp: %t", got, item.exp)
 			}
 
@@ -54,13 +54,13 @@ func TestValidAnagram (t *testing.T) {
 
 }
 
-func TestConcatenationOfArray(t *testing.T)  {
+func TestConcatenationOfArray(t *testing.T) {
 
-	test_items := []struct{
-		id int
+	test_items := []struct {
+		id   int
 		nums []int
-		exp []int
-	} {
+		exp  []int
+	}{
 		{1, []int{1, 2, 3}, []int{1, 2, 3, 1, 2, 3}},
 		{2, []int{1, 2}, []int{1, 2, 1, 2}},
 	}
@@ -69,7 +69,7 @@ func TestConcatenationOfArray(t *testing.T)  {
 		t.Run(strconv.Itoa(test_item.id), func(t *testing.T) {
 			got := ConcatenationOfArray(test_item.nums)
 
-			if (!reflect.DeepEqual(got, test_item.exp)) {
+			if !reflect.DeepEqual(got, test_item.exp) {
 				t.Errorf("got:%#v, exp: %#v", got, test_item.exp)
 			}
 
@@ -77,38 +77,34 @@ func TestConcatenationOfArray(t *testing.T)  {
 	}
 }
 
-
-
-
-func TestGreatestRightSide(t *testing.T)  {
+func TestGreatestRightSide(t *testing.T) {
 	test_items := []struct {
-		id int
+		id  int
 		arr []int
 		exp []int
-	} {
-		{1, []int{17, 18, 5, 4, 6, 1}, []int {18, 6, 6, 6, 1, -1}},
-		{2, []int{400}, []int {-1}},
+	}{
+		{1, []int{17, 18, 5, 4, 6, 1}, []int{18, 6, 6, 6, 1, -1}},
+		{2, []int{400}, []int{-1}},
 	}
 
 	for _, test_item := range test_items {
 		t.Run(strconv.Itoa(test_item.id), func(t *testing.T) {
 			got := GreatestRightSide(test_item.arr)
-			if (!reflect.DeepEqual(got, test_item.exp)) {
+			if !reflect.DeepEqual(got, test_item.exp) {
 				t.Errorf("got: %#v, exp: %#v", got, test_item.exp)
 			}
 
 		})
-	
+
 	}
 }
 
-
-func TestIsSubsequence(t *testing.T)  {
-	test_items := []struct{
-		id int
+func TestIsSubsequence(t *testing.T) {
+	test_items := []struct {
+		id   int
 		s, t string
-		exp bool
-	} {
+		exp  bool
+	}{
 		{1, "abc", "ahbgdc", true},
 		{2, "axc", "ahbgdc", false},
 	}
@@ -123,14 +119,12 @@ func TestIsSubsequence(t *testing.T)  {
 	}
 }
 
-
-
-func TestLengthOfLastWord(t *testing.T)  {
-	test_items := []struct{
-		id int
-		s string
+func TestLengthOfLastWord(t *testing.T) {
+	test_items := []struct {
+		id  int
+		s   string
 		exp int
-	} {
+	}{
 		{1, "Hello World", 5},
 		{2, "   fly me   to   the moon  ", 4},
 		{3, "luffy is still joyboy", 6},
@@ -146,13 +140,13 @@ func TestLengthOfLastWord(t *testing.T)  {
 	}
 }
 
-func TestTwoSum(t *testing.T)  {
-	test_items := []struct{
-		id int
-		nums []int
+func TestTwoSum(t *testing.T) {
+	test_items := []struct {
+		id     int
+		nums   []int
 		target int
-		exp [2]int
-	} {
+		exp    [2]int
+	}{
 		{1, []int{2, 7, 11, 15}, 9, [2]int{0, 1}},
 		{2, []int{3, 2, 4}, 6, [2]int{1, 2}},
 		{3, []int{3, 3}, 6, [2]int{0, 1}},
@@ -162,21 +156,19 @@ func TestTwoSum(t *testing.T)  {
 
 		t.Run(strconv.Itoa(item.id), func(t *testing.T) {
 			got := TwoSum(item.nums, item.target)
-			if (!reflect.DeepEqual(got, item.exp)) {
+			if !reflect.DeepEqual(got, item.exp) {
 				t.Errorf("got: %#v, exp: %#v", got, item.exp)
 			}
 		})
 	}
 }
 
-
-
-func TestLongestCommonPrefix(t *testing.T)  {
-	test_items := []struct{
-		id int
-		s []string
+func TestLongestCommonPrefix(t *testing.T) {
+	test_items := []struct {
+		id  int
+		s   []string
 		exp string
-	} {
+	}{
 		{1, []string{"flower", "flow", "flight"}, "fl"},
 		{1, []string{"dog", "racecar", "car"}, ""},
 	}
@@ -191,14 +183,12 @@ func TestLongestCommonPrefix(t *testing.T)  {
 	}
 }
 
-
-
-func TestPascalsTriangle(t *testing.T)  {
-	test_items := []struct{
-		id int
+func TestPascalsTriangle(t *testing.T) {
+	test_items := []struct {
+		id      int
 		numRows int
-		exp [][]int
-	} {
+		exp     [][]int
+	}{
 		{1, 5, [][]int{{1}, {1, 1}, {1, 2, 1}, {1, 3, 3, 1}, {1, 4, 6, 4, 1}}},
 		{2, 1, [][]int{{1}}},
 	}
@@ -206,24 +196,90 @@ func TestPascalsTriangle(t *testing.T)  {
 	for _, item := range test_items {
 		t.Run(strconv.Itoa(item.id), func(t *testing.T) {
 			got := PascalsTriangle(item.numRows)
-			if (!reflect.DeepEqual(got, item.exp)){
+			if !reflect.DeepEqual(got, item.exp) {
 				t.Errorf("got: %#v, exp: %#v", got, item.exp)
 			}
 		})
 	}
 }
 
+func TestRemoveElement(t *testing.T) {
+	testing_items := []struct {
+		id   int
+		nums []int
+		val  int
+		exp  []int
+	}{
+		{id: 1, nums: []int{3, 2, 2, 3}, val: 3, exp: []int{2, 2, 3, 3}},
+		{id: 2, nums: []int{0, 1, 2, 2, 3, 0, 4, 2}, val: 2, exp: []int{0, 1, 4, 0, 3, 2, 2, 2}},
+	}
 
+	for _, item := range testing_items {
+		t.Run(strconv.Itoa(item.id), func(t *testing.T) {
+			got := RemoveElement(item.nums, item.val)
+			if !reflect.DeepEqual(got, item.exp) {
+				t.Errorf("got: %#v, exp: %#v", got, item.exp)
+			}
+		})
+	}
 
+}
 
+func TestValidEmails(t *testing.T) {
+	test_items := []struct {
+		id     int
+		emails []string
+		exp    int
+	}{
+		{id: 1, emails: []string{"test.email+alex@leetcode.com", "test.e.mail+bob.cathy@leetcode.com", "testemail+david@lee.tcode.com"}, exp: 2},
+		{id: 2, emails: []string{"a@leetcode.com", "b@leetcode.com", "c@leetcode.com"}, exp: 3},
+	}
 
+	for _, item := range test_items {
+		got := ValidEmails(item.emails)
+		if got != item.exp {
+			t.Errorf("got: %d, exp: %d", got, item.exp)
+		}
+	}
+}
 
+func TestIsomorphicStrings(t *testing.T) {
+	test_items := []struct {
+		id   int
+		s, t string
+		exp  bool
+	}{
+		{id: 1, s: "egg", t: "add", exp: true},
+		{id: 2, s: "foo", t: "bar", exp: false},
+		{id: 3, s: "paper", t: "title", exp: true},
+		{id: 4, s: "bbbaaaba", t: "aaabbbba", exp: false},
+	}
 
+	for _, item := range test_items {
+		got := IsomorphicStrings(item.s, item.t)
+		if got != item.exp {
+			t.Errorf("s: %s, t: %s, got: %t, exp: %t", item.s, item.t, got, item.exp)
+		}
+	}
+}
 
+func TestCanPlantFlower(t *testing.T) {
+	test_items := []struct {
+		id        int
+		flowerbed []int
+		n         int
+		exp       bool
+	}{
+		{id: 1, flowerbed: []int{1, 0, 0, 0, 1}, n: 1, exp: true},
+		{id: 2, flowerbed: []int{1, 0, 0, 0, 1}, n: 2, exp: false},
+		{id: 3, flowerbed: []int{0}, n: 1, exp: true},
+		{id: 4, flowerbed: []int{0, 0}, n: 2, exp: false},
+	}
 
-
-
-
-
-
-
+	for _, item := range test_items {
+		got := CanPlantFlower(item.flowerbed, item.n)
+		if got != item.exp {
+			t.Errorf("flowerbed: %#v, n: %d, got: %t, exp: %t, id: %d", item.flowerbed, item.n, got, item.exp, item.id)
+		}
+	}
+}
