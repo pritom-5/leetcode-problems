f5a8de0a92d9252c056e96a7d664ab9493a239ab
pritom-lap <iamsakibshadmanpritom@gmail.com>
2024-05-13 22:14:13 +0600

    07

---
+++ go/slidingWindow/slidingWindow.go
@@ -0,0 +1,17 @@
+package slidingwindow
+
+import "math"
+
+func MaxProfit(prices []int) int  {
+	min := math.MaxInt32
+	diff := 0
+
+	for _, price := range prices {
+		if price > min {
+			diff = max(price - min, diff)
+		} else {
+			min = price
+		}
+	}
+	return diff
+}
