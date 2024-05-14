c88074f6a42c59407ea0ef381aee3fe839a12b0a
pritom-lap <iamsakibshadmanpritom@gmail.com>
2024-05-14 12:02:29 +0600

    08

---
+++ go/stack/stack.go
@@ -0,0 +1,101 @@
+package stack
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func IsValid(s string) bool  {
+
+	samples := map[string]struct{} {
+		"()": {},
+		"[]": {},
+		"{}": {},
+	}
+
+	s_arr := []rune(s)
+
+	paren_stack := make([]rune, 0)
+
+	for i := len(s_arr) - 1; i >= 0; i-- {
+		curr := s_arr[i]
+
+		if len(paren_stack) == 0 {
+			paren_stack = append(paren_stack, curr)
+			continue
+		}
+
+		last_popped_paren := paren_stack[len(paren_stack) - 1]
+		tmp_combined_paren := string(curr) + string(last_popped_paren)
+
+		if _, ok := samples[tmp_combined_paren]; ok {
+			paren_stack = paren_stack[:len(paren_stack) - 1]
+			continue
+		}
+
+		paren_stack = append(paren_stack, curr)
+		
+		fmt.Printf("paren_stack: %#v\n", paren_stack)
+	}
+
+	return len(paren_stack) == 0
+}
+
+func getSumOfArray(arr []string) int {
+	total := 0
+
+	for _, item := range arr {
+		item_int, err := strconv.Atoi(item)
+		if err != nil {
+			fmt.Printf("item: %s", item)
+			continue
+		}
+		total += item_int
+	}
+	return total
+}
+
+func BaseballGame(ops []string) int  {
+	stack := make([]string, 0)
+
+	for _, op := range ops {
+		switch op {
+		case "C":
+			stack = stack[:len(stack) - 1]
+
+		case "D":
+			last_int_item, err := strconv.Atoi(stack[len(stack) - 1])
+			if err != nil {
+				continue
+			}
+			doubled := last_int_item * 2
+			stack = append(stack, strconv.Itoa(doubled))
+
+		case "+":
+			last_int_item, err := strconv.Atoi(stack[len(stack) - 1])
+			if err != nil {
+				continue
+			}
+			second_last_int_item, err := strconv.Atoi(stack[len(stack) - 2])
+			if err != nil {
+				continue
+			}
+			stack = append(stack, strconv.Itoa(last_int_item + second_last_int_item))
+
+		default:
+			stack = append(stack, op)
+}
+
+	}
+	
+	return getSumOfArray(stack)
+}
+
+
+
+
+
+
+
+
+
---
+++ go/stack/stack_test.go
@@ -0,0 +1,44 @@
+package stack
+
+import "testing"
+
+func TestIsValid(t *testing.T)  {
+	
+	testing_data := []struct{
+		id int
+		s string
+		exp bool
+	} {
+		{id: 1, s: "()", exp: true},
+		{id: 2, s: "()[]{}", exp: true},
+		{id: 3, s: "(]", exp: false},
+	}
+
+	for _, item := range testing_data {
+		got := IsValid(item.s)
+		if got != item.exp {
+			t.Errorf("s: %s, got: %t, exp: %t",item.s, got, item.exp)
+		}
+	}
+}
+
+
+func TestBaseballGame(t *testing.T)  {
+	
+	testing_data := []struct{
+		id, exp int
+		ops []string
+	} {
+		{id: 1, ops: []string{"5","2","C","D","+"}, exp: 30},
+		{id: 2, ops: []string{"5","-2","4","C","D","9","+","+"}, exp: 27},
+		{id: 3, ops: []string{"1","C"}, exp: 0},
+	}
+
+	for _, item := range testing_data {
+		got := BaseballGame(item.ops)
+		if got != item.exp {
+			t.Errorf("got: %d, exp: %d", got, item.exp)
+		}
+	}
+}
+
