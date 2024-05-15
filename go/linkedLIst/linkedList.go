238222c648557e1324c423453626492a012b7430
pritom-lap <iamsakibshadmanpritom@gmail.com>
2024-05-15 14:28:40 +0600

    12

--- go/linkedLIst/linkedList.go
+++ go/linkedLIst/linkedList.go
@@ -1,6 +1,7 @@
 package linkedlist
 
 
+
 type linkedListNode struct {
 	value int
 	next *linkedListNode
@@ -42,3 +43,66 @@ func (ll *linkedList) ReverseLinkedList () {
 
 	ll.head = prev
 }
+
+
+func createLinkedListFromSlice (values []int) *linkedList {
+	linked_list := &linkedList{}
+	for _, val := range values {
+		linked_list.AddNode(val)
+	}
+	return linked_list
+}
+
+func mergeLinkedList(ll_1, ll_2 *linkedList) *linkedList {
+	var ll_1_node, ll_2_node *linkedListNode = ll_1.head, ll_2.head
+	new_ll_slice := make([]int, 0)
+
+	for ll_1_node != nil && ll_2_node != nil {
+			if ll_1_node.value >= ll_2_node.value {
+				new_ll_slice = append(new_ll_slice, ll_2_node.value)
+				ll_2_node = ll_2_node.next
+				
+			} else {
+				new_ll_slice = append(new_ll_slice, ll_1_node.value)
+				ll_1_node = ll_1_node.next
+			}
+	}
+
+	for ll_1_node != nil {
+			new_ll_slice = append(new_ll_slice, ll_1_node.value)
+			ll_1_node = ll_1_node.next
+	}
+	for ll_2_node != nil {
+			new_ll_slice = append(new_ll_slice, ll_2_node.value)
+			ll_2_node = ll_2_node.next
+	}
+
+	return createLinkedListFromSlice(new_ll_slice)
+}
+
+func IsPalindrome (nums []int) bool {
+	left, right := 0, len(nums) - 1 
+
+	for left < right {
+		if nums[left] != nums[right] {
+			return false
+		}
+		left += 1
+		right -= 1
+	}
+	return true
+}
+
+func IsPalindromeLinkedList(linked_list *linkedList) bool  {
+	linked_list_value_slice := make([]int, 0)
+	
+	curr := linked_list.head
+
+	for curr != nil {
+		linked_list_value_slice = append(linked_list_value_slice, curr.value)
+		curr = curr.next
+	}
+
+	return IsPalindrome(linked_list_value_slice)
+}
+
--- go/linkedLIst/linkedList_test.go
+++ go/linkedLIst/linkedList_test.go
@@ -1,14 +1,14 @@
 package linkedlist
 
 import (
-	"fmt"
+	"reflect"
 	"testing"
 )
 
+
 func TestAddNode(t *testing.T)  {
-	linked_list := linkedList{}
-	linked_list.AddNode(10)
-	linked_list.AddNode(20)
+	linked_list := createLinkedListFromSlice([]int{10, 20})
+
 
 	value_1 := linked_list.head.value
 	value_2 := linked_list.head.next.value
@@ -22,11 +22,7 @@ func TestAddNode(t *testing.T)  {
 }
 
 func TestReverseLinkedList(t *testing.T)  {
-	linked_list_01 := linkedList{}
-	fmt.Print(linked_list_01)
-	linked_list_01.AddNode(10)
-	linked_list_01.AddNode(20)
-	linked_list_01.AddNode(30)
+	linked_list_01 := createLinkedListFromSlice([]int{10, 20, 30})
 
 	linked_list_01.ReverseLinkedList()
 
@@ -39,5 +35,64 @@ func TestReverseLinkedList(t *testing.T)  {
 	if linked_list_01.head.next.next.value != 10 {
 		t.Errorf("got: %d, exp: %d", linked_list_01.head.next.next.value, 10)
 	}
+}
+
+func TestMergeLinkedList(t *testing.T)  {
+	ll_1 := createLinkedListFromSlice([]int {1, 2, 4})
+	ll_2 := createLinkedListFromSlice([]int {1, 3, 4})
+
+	ll_3 := createLinkedListFromSlice([]int {1, 1, 2, 3, 4, 4})
+
+	merged_linked_list := mergeLinkedList(ll_1, ll_2)
+
+	if !reflect.DeepEqual(ll_3, merged_linked_list) {
+		t.Errorf("merged_linked_list: %#v", merged_linked_list)
+	}
+}
+
+func TestIsPalindrome(t *testing.T)  {
+	test_data_items := []struct{
+		id string
+		nums []int
+		exp bool
+	}{
+		{id: "1", nums: []int{1, 2, 3, 2, 1}, exp: true},
+		{id: "2", nums: []int{1, 2, 2, 1}, exp: true},
+		{id: "3", nums: []int{1, 3,  2, 1}, exp: false},
+		{id: "4", nums: []int{}, exp: true},
+	}
+
+	for _, item := range test_data_items {
+		t.Run(item.id, func(t *testing.T) {
+			got := IsPalindrome(item.nums)
+			if got != item.exp {
+				t.Errorf("got: %t, exp: %t", got, item.exp)
+			}
+
+		})
+	}
 	
 }
+	func TestIsPalindromeLinkedList(t *testing.T)  {
+		test_items := []struct{
+			id string
+			linked_list *linkedList
+			exp bool
+		}{
+			{id: "1", linked_list: createLinkedListFromSlice([]int{1, 2, 2, 1}), exp: true},
+			{id: "2", linked_list: createLinkedListFromSlice([]int{1, 2}), exp: false},
+		}
+
+	for _, item := range test_items {
+		t.Run(item.id, func(t *testing.T) {
+			got := IsPalindromeLinkedList(item.linked_list)
+
+			if got != item.exp {
+				t.Errorf("got: %t, exp: %t", got, item.exp)
+			}
+
+		})
+	}
+	}
+
+
