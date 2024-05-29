912e99377641d5c2b9e700c714d59d9dd7b72506
pritom-lap <iamsakibshadmanpritom@gmail.com>
2024-05-29 21:00:03 +0600

    26

--- go/linkedList/linkedListUtils.go
+++ go/linkedList/linkedListUtils.go
@@ -51,3 +51,21 @@ func (linked_list *linkedList) sliceFromLinkedList() []int {
 
 	return slice
 }
+
+func (head *linkedListNode) sliceFromLinkedList() []int {
+	s := make([]int, 0)
+	if head == nil {
+		return s
+	}
+
+	for head != nil {
+		s = append(s, head.value)
+		head = head.next
+	}
+
+
+	return s
+}
+
+
+
