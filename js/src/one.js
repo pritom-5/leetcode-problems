82592a0a014d8798075b078854edf8622f6a8d0b
pritom-lap <iamsakibshadmanpritom@gmail.com>
2024-11-09 23:40:49 +0600

    34

--- js/src/one.js
+++ js/src/one.js
@@ -15,3 +15,20 @@ function createNewInfo (info) {
 	return info.firstName + info.secondName + " -- " + info.age.toString();
 
 }
+
+/** @param {string} s */
+function validPallindrome (s) {
+	const reversed = s.split("").map(item => {
+		const c = item.toLowerCase().charCodeAt(0);
+		if (c >= 98 && c <= 122) {
+			return item
+		} 
+			return ""
+		
+	}).toReversed().join("");
+
+	return reversed === s;
+}
+
+const t = "a man is, lying on the floor";
+validPallindrome(t);
